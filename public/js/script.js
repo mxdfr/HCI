56c8603bc8225ef0b934845ac268ac1eba96b5db
Ramonprogramming <ramon.dijkstra@hotmail.com>
2019-06-18 15:15:51 +0200

    Fixed accordion

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,18 @@
 $( document ).ready(function() {
-    var coll = document.getElementsByClassName("white-block");
-    var i;
-    
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight){
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + "px";
-        } 
-      });
-    }
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
 });
 
 function login() {
